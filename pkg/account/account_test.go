72d2666d7e5b8cf5a7690789269d790a20cb27ec
Harry <harryjp1995@gmail.com>
2018-11-03 20:04:14 +0000

    test: Adds test for Withdraw method

--- pkg/account/account_test.go
+++ pkg/account/account_test.go
@@ -10,3 +10,11 @@ func TestAccountZeroBalance(t *testing.T) {
 		t.Errorf("Test failed -> Expected balance to be zero, but was %f", acc.Balance())
 	}
 }
+
+func TestAccountWithdraw(t *testing.T) {
+	acc := NewAccount(100.00)
+	acc.Withdraw(25.50)
+	if acc.Balance() != 74.50 {
+		t.Errorf("Test failed -> Expected balance to be 74.50, but was %f", acc.Balance())
+	}
+}
